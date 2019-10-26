8413a942d4d422adbea190ea4fad843c50aca202
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-10-26 23:53:34 +0300

    Implement mount as loopback device

--- src/raspi_config.py
+++ src/raspi_config.py
@@ -38,7 +38,45 @@ def create_folder(path):
     return subprocess.run(["mkdir", "-p", path]).returncode
 
 
-def prepare(img_path):
+def process_fdisk_output(img_path):
+    """Process fdisk output to get loop offsets for mounting partitions.
+
+    Args:
+        img_path: The path of the img file.
+
+    Returns:
+        A tuple of type (boot_offset, root_offset)
+    """
+
+    fdisk_output = subprocess.check_output(["fdisk", "-l", img_path])
+    fdisk_lines = fdisk_output.splitlines()
+    fdisk_lines = [line.decode("utf-8") for line in fdisk_lines]
+
+    for line in fdisk_lines:
+        start = line.split(" ")[0]
+        # Sector size
+        if start == "Units:":
+            sector_size = int(line.split("=")[-1].split("bytes")[0])
+        # Start of boot partition
+        elif start == (img_path.split('/')[-1] + "1"):
+            for el in line.split(" ")[1:]:
+                if el != "":
+                    boot_start = int(el)
+                    break
+        # Start of root partition
+        elif start == (img_path.split('/')[-1] + "2"):
+            for el in line.split(" ")[1:]:
+                if el != "":
+                    root_start = int(el)
+                    break
+
+    boot_offset = sector_size * boot_start
+    root_offset = sector_size * root_start
+
+    return boot_offset, root_offset
+
+
+def prepare(img_path): 
     """Create folder and mount image.
 
     Args:
@@ -57,8 +95,41 @@ def prepare(img_path):
                            " with exit status {}".format(status))
 
 
+def mount_partition(img_path, target_path, offset):
+    """Mount a raspberry partition to a specific folder.
+
+    Args:
+        img_path: The path of image file.
+        target_path: The folder where the partition will be mounted.
+        offset: The loopback offset.
+
+    Returns:
+        Status code of command.
+    """
+
+    status = subprocess.run(["sudo", "mount", "-o", "loop,offset="+str(offset),
+                             img_path, target_path])
+
+    return status
+
+
+def unmount(target_path):
+    """Unmount a partition.
+
+    Args:
+        target_path: The folder where the partition is mounted.
+
+    Returns:
+        Status code of command.
+    """
+
+    status = subprocess.run(["sudo", "umount", target_path])
+    return status
+
+
 def clean_up():
     """Clean up folders."""
+
     return subprocess.run(["rm", "-rf", tmp_path])
 
 
@@ -67,9 +138,15 @@ if __name__ == "__main__":
 
     args = parse_args()
 
-    prepare()
+    # Prepare folders
+    prepare(args.image)
+
+    # Get offsets of partitions.
+    boot_offset, root_offset = process_fdisk_output(args.image)
+    #mount_partition(args.image, boot_path, boot_offset)
+    mount_partition(args.image, root_path, root_offset)
 
     # Clean up
-    status = clean_up()
-    if not status:
-        raise RuntimeError("Rm failed with exit status {}".format(status))
+    #status = clean_up()
+    #if not status:
+    #    raise RuntimeError("Rm failed with exit status {}".format(status))
